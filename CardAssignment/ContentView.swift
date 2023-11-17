480e71ea7df2424a830f5184ec86de681c255b84
Trsh16 <tlikorawung@student.ciputra.ac.id>
2023-11-18 00:14:52 +0700

    selesai

--- CardAssignment/ContentView.swift
+++ CardAssignment/ContentView.swift
@@ -89,79 +89,91 @@ struct SearchBar: View {
     }
 }
 
-//struct CardView: View {
-//    var card: Card
-//
-//        let columns = [
-//            GridItem(.flexible(), spacing: 16),
-//            GridItem(.flexible(), spacing: 16)
-//        ]
-//
-//        var body: some View {
-//            ScrollView {
-//                LazyVGrid(columns: columns, spacing: 16) {
-//                    ForEach(card.data, id: \.id) { datum in
-//                        NavigationLink(destination: CardDetailView(datum: datum)) {
-//                            DatumView(datum: datum)
-//                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)             
-//                        }
-//                    }
-//                }
-//                .padding(16)
-//            }
-//        }
-//}
+
 struct CardDetailView: View {
+    @State private var isImageTapped = false
+
     var datum: Datum
 
     var body: some View {
         VStack {
             ScrollView {
-                VStack() {
-                    // Use a single AsyncImage
-                    
-                    VStack {
-                        AsyncImage(url: URL(string: datum.imageUris?.artCrop ?? "")) { phase in
-                            switch phase {
-                            case .success(let image):
-                                image
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(maxWidth: .infinity)
-                                                           .frame(height: 100, alignment: .top) // Set height and align to the top
-                            case .failure:
-                                Text("Failed to load image")
-                            case .empty:
-                                // Provide a placeholder image for the empty state
-                                Image(systemName: "photo")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(maxWidth: .infinity)
-                                                           .frame(height: 100, alignment: .top) // Set height and align to the top
-                            @unknown default:
-                                EmptyView()
-                            }
+                VStack(alignment: .leading, spacing: 100) {
+
+                    // Use a single AsyncImage with a TapGesture
+                    AsyncImage(url: URL(string: datum.imageUris?.artCrop ?? "")) { phase in
+                        switch phase {
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 100, alignment: .top)
+                                .onTapGesture {
+                                    // Set a flag to show a larger image when tapped
+                                    isImageTapped = true
+                                }
+                        case .failure:
+                            Text("Failed to load image")
+                        case .empty:
+                            Image(systemName: "photo")
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 100, alignment: .top)
+                                .onTapGesture {
+                                    isImageTapped = true
+                                }
+                        @unknown default:
+                            EmptyView()
                         }
                     }
 
-
                     Spacer()
 
-                    // Move the details below the image
-                    VStack() {
+                    VStack(alignment: .leading) {
                         Text("Name: \(datum.name)").bold()
                         Text("Type Line: \(datum.typeLine.rawValue)")
                         Text("Mana Cost: \(datum.manaCost)")
-
+                        Text("LEGALITIES").bold()
                         LegalitiesView(legalities: datum.legalities)
-
-                        // Add more Text views for other properties you want to display
                     }
-           
-
+                    .padding()
                 }
                 .navigationBarTitle(datum.name, displayMode: .inline)
             }
+        }
+        .sheet(isPresented: $isImageTapped) {
+            // This is the sheet that will appear when the image is tapped
+            LargerImageView(url: URL(string: datum.imageUris?.normal ?? ""))
+        }
+    }
+}
+
+struct LargerImageView: View {
+    var url: URL?
+
+    var body: some View {
+        // Create a view to display the larger image
+        if let url = url {
+            AsyncImage(url: url) { phase in
+                switch phase {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                case .failure:
+                    Text("Failed to load larger image")
+                case .empty:
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+        } else {
+            Text("Invalid URL")
         }
     }
 }
@@ -173,17 +185,27 @@ struct LegalitiesView: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
-           Text("Standard: \(legalities.standard.rawValue)")
+            Text("Standard: \(legalities.standard.rawValue)")
             Text("Modern: \(legalities.modern.rawValue)")
             Text("Vintage: \(legalities.vintage.rawValue)")
             Text("Alchemy: \(legalities.alchemy.rawValue)")
-            Text("Modern: \(legalities.modern.rawValue)")
-            Text("Vintage: \(legalities.vintage.rawValue)")
-            Text("Alchemy: \(legalities.alchemy.rawValue)")
-            Text("Modern: \(legalities.modern.rawValue)")
-            Text("Vintage: \(legalities.vintage.rawValue)")
-            Text("Alchemy: \(legalities.alchemy.rawValue)")
-            // Add more legalities as needed
+            Text("Brawl: \(legalities.brawl.rawValue)")
+            Text("Commander: \(legalities.commander.rawValue)")
+            Text("Duel: \(legalities.duel.rawValue)")
+            Text("Explorer: \(legalities.explorer.rawValue)")
+            Text("Future: \(legalities.future.rawValue)")
+            Text("Gladiator: \(legalities.gladiator.rawValue)")
+            Text("Historic: \(legalities.historic.rawValue)")
+            Text("HistoricBrawl: \(legalities.historicbrawl.rawValue)")
+            Text("Legacy: \(legalities.legacy.rawValue)")
+            Text("Oathbreaker: \(legalities.oathbreaker.rawValue)")
+            Text("Oldschool: \(legalities.oldschool.rawValue)")
+            Text("Pauper: \(legalities.pauper.rawValue)")
+            Text("PauperCommander: \(legalities.paupercommander.rawValue)")
+            Text("Penny: \(legalities.penny.rawValue)")
+            Text("Pioneer: \(legalities.pioneer.rawValue)")
+            Text("Predh: \(legalities.predh.rawValue)")
+            Text("Premodern: \(legalities.premodern.rawValue)")
         }
     }
 }
