c7c96a9841cdfd269aa8e890046a9a053a8e32dd
Trsh16 <tlikorawung@student.ciputra.ac.id>
2023-11-17 21:55:19 +0700

    Initial Commit

---
+++ CardAssignment/CardAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAssignmentApp.swift
+//  CardAssignment
+//
+//  Created by MacBook Pro on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CardAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
